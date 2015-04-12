ace2d290d2713dabb5ab6b2805adf7ad0ffa08e7
JiangXian <jiangxian@JiangXiandeMacBook-Pro.local>
2015-04-12 22:30:04 +0800

    test

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 	define('APP_NAME', 'JUAOL');     //项目名称
 	define('APP_PATH', './JUAOL/');  //项目路径
 	define('APP_DEBUG', true);          //开启调试模式
-
+//hah
 	require './ThinkPHP/ThinkPHP.php';  //引入ThinkPHP核心运行文件
 ?>
