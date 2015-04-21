5ae27966c10cf8b50c05e79a479617a495e3cd3a
jiangxian <jiangxian@meituan.com>
2015-04-22 00:25:12 +0800

    添加访问控制

--- JUAOL/Lib/Action/AdminAction.class.php
+++ JUAOL/Lib/Action/AdminAction.class.php
@@ -1,6 +1,11 @@
 <?php
 class AdminAction extends Action {
     public function index(){
-    	$this->assign($data)->display('admin');
+    	if(session('user') != null){
+    		$this->assign($data)->display('admin');
+    	}
+    	else{
+    		$this->error("请登录后在进行操作！");
+    	}
     }
 }
--- JUAOL/Lib/Action/UserAction.class.php
+++ JUAOL/Lib/Action/UserAction.class.php
@@ -4,75 +4,85 @@ import('ORG.Net.UploadFile');
 
 class UserAction extends Action {
     public function index(){
-    	$data['personInfo'] = D('Person')->getPersonInfoList();
-    	$data['team'] = session('user')['team'];
-        $data['username'] = session('user')['username'];
-    	$data['groupe'] = array(
-    		'Catering',
-    		'Competitor',
-    		'Judoka',
-    		'Coach',
-    		'Team-Official',
-    		'Referee',
-    		'Doctor',
-    		'Medic',
-    		'Physiotherapist',
-    		'President',
-    		'Vice-President',
-    		'General Secretary',
-    		'Delegate',
-    		'LJF Staff',
-    		'VIP',
-    		'VVIP',
-    		'Head of Organisation'
-    		);
-    	$this->assign($data)->display('user_manage');
+        if(session('user') != null){
+        	$data['personInfo'] = D('Person')->getPersonInfoList();
+        	$data['team'] = session('user')['team'];
+            $data['username'] = session('user')['username'];
+        	$data['groupe'] = array(
+        		'Catering',
+        		'Competitor',
+        		'Judoka',
+        		'Coach',
+        		'Team-Official',
+        		'Referee',
+        		'Doctor',
+        		'Medic',
+        		'Physiotherapist',
+        		'President',
+        		'Vice-President',
+        		'General Secretary',
+        		'Delegate',
+        		'LJF Staff',
+        		'VIP',
+        		'VVIP',
+        		'Head of Organisation'
+        		);
+        	$this->assign($data)->display('user_manage');
+        }
+        else{
+            $this->error("请登录后在进行操作！");
+        }
     }
     public function addPerson(){
+        if(session('user') != null){
 
-    	$upload = new UploadFile();
-    	$upload->maxSize = 3145728 ;// 设置附件上传大小
-		$upload->allowExts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        	$upload = new UploadFile();
+        	$upload->maxSize = 3145728 ;// 设置附件上传大小
+    		$upload->allowExts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
 
-		$upload->savePath = './JUAOL/Resource/img/person_img/';
-		$upload->saveRule = time().'_'.mt_rand();
+    		$upload->savePath = './JUAOL/Resource/img/person_img/';
+    		$upload->saveRule = time().'_'.mt_rand();
 
-		
-		// dump($info);die;
-    	$person = D("Person");
+    		
+    		// dump($info);die;
+        	$person = D("Person");
 
-    	foreach($person->getFields() as $attr){
-    		// echo $attr;s
-    		$tmp = $_POST[$attr];
-    		if($tmp != NULL)
- 	   			$data[$attr] = $_POST[$attr];
-    	}
-    	if($_POST['id'] != '')
-    		$data['id'] = $_POST['id'];
+        	foreach($person->getFields() as $attr){
+        		// echo $attr;s
+        		$tmp = $_POST[$attr];
+        		if($tmp != NULL)
+     	   			$data[$attr] = $_POST[$attr];
+        	}
+        	if($_POST['id'] != '')
+        		$data['id'] = $_POST['id'];
 
-    	if($upload->upload()) {
-			$info =  $upload->getUploadFileInfo();
-            $data['img_url'] = C('TMPL_PARSE_STRING')['__PUBLIC__'].'/img/person_img/'.$info[0]['savename'];
-		}
-		// dump($person->fields);
-		// dump($info[0]['savename']);
-		// $person->create();
-		// dump($data);die;
-		
-		if($data['id'] == ''){
-            if (false !== $person->addPerson($data)) {
-                $this->success('数据添加成功！');
-            } else {
-                $this->error('数据写入错误');
-            }
-	    }
-	    else{
-	    	if (false !== $person->updatePerson($data)) {
-                $this->success('数据更新成功！');
-            } else {
-                $this->error('数据写入错误');
-            }
-	    }
+        	if($upload->upload()) {
+    			$info =  $upload->getUploadFileInfo();
+                $data['img_url'] = C('TMPL_PARSE_STRING')['__PUBLIC__'].'/img/person_img/'.$info[0]['savename'];
+    		}
+    		// dump($person->fields);
+    		// dump($info[0]['savename']);
+    		// $person->create();
+    		// dump($data);die;
+    		
+    		if($data['id'] == ''){
+                if (false !== $person->addPerson($data)) {
+                    $this->success('数据添加成功！');
+                } else {
+                    $this->error('数据写入错误');
+                }
+    	    }
+    	    else{
+    	    	if (false !== $person->updatePerson($data)) {
+                    $this->success('数据更新成功！');
+                } else {
+                    $this->error('数据写入错误');
+                }
+    	    }
+        }
+        else{
+            $this->error("请登录后在进行操作！");
+        }
         
     }
 
